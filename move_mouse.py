58f6971b6279c2ac343895cc37b2c893f9871aed
Cedric Honnet <honnet@telecom-paristech.org>
2015-12-13 01:08:29 +0800

    Restart with class.

--- move_mouse.py
+++ move_mouse.py
@@ -1,38 +1,70 @@
 from oscAPI import listen, readQueue, bind, init
 from pymouse import PyMouse
 from math import cos, sin
-M = PyMouse()
-W, H = M.screen_size()
-# mx, my = M.position()
+import sys, select
 
-x_min = 0.535187304020 # left
-x_max = 0.616868846118 # right
+# OSC instructions: send 'rz_d' (as X) and 'az_d' (as Y) TODO verify Y
+# host='127.0.0.1', port=3333, OSCaddress='/update'
 
-y_min = 0.478545814753 # low
-y_max = 0.584199279547 # high
+class Maus(): # Maus(host='127.0.0.1', port=3333, OSCaddress='/update') # TODO?
+    M = PyMouse()
+    W, H = M.screen_size()      # TODO: use if saturation too
+    x_pos, y_pos = M.position() # useful to start from current position TODO: use it!
+
+    isCalibrated = False
+    x_min = +sys.float_info.max # left
+    x_max = -sys.float_info.max # right
+    y_min = +sys.float_info.max # low
+    y_max = -sys.float_info.max # high
+
+    def __init__(self, host='127.0.0.1', port=3333, OSCaddress='/update'):
+        oscAPI.init()
+        oscid = oscAPI.listen(host, port)
+        oscAPI.bind(oscid, self.update_imu_data, OSCaddress)
+        print "Listening to IP:", host, "- port:", port, "- OSC address:", OSCaddress
+
+    def update_imu_data(self, message, *args):
+        # TODO: use current mouse position
+        self.x_pos, self.y_pos = message[2:] # value received from the twiz
+
+        if self.isCalibrated:
+            self.x_pos = ( (self.x_pos + 0.5 - self.x_min) / (self.x_max - self.x_min) ) * W
+            self.y_pos = ( (self.y_pos + 0.5 - self.y_min) / (self.y_max - self.x_min) ) * H
+            # TODO : invert y, smoothen, improve range...
+
+            maus.move()
+        else:
+            self.x_pos = ( self.x_pos + 0.5 ) * W
+            self.y_pos = ( self.y_pos + 0.5 ) * H
+            print self.x_pos, self.y_pos                      # TODO: VERIFY THIS !!!!!!!!!!!!!!!!!
+
+    def calibrate(self):
+        print "Move in the wanted range (up + down) and press ENTER..."
+
+        done = False
+        while not done:
+            done, x, x = select.select( [sys.stdin], [], [], .01 ) # 10ms timeout
+
+            self.x_min = min(self.x_pos, self.x_min)
+            self.y_min = min(self.y_pos, self.y_min)
+
+            self.x_max = max(self.x_pos, self.x_max)
+            self.y_max = max(self.y_pos, self.y_max)
+
+        print "Calibration done - X:", self.x_min, self.x_max , "Y:", self.y_min, self.y_max
+        self.isCalibrated = True
+
+    def move(self):
+        # M.move(x_pos, y_pos) # TODO
+        print "Moving to:", x_pos, '\t', y_pos
+
 
 def main():
-    init()
-    host='192.168.42.68'
-    oscid = listen(host, port=3333)
-    bind(oscid, update_mouse, '/update')
-
-    # compute the 4 max values and the ranges
+    maus = Maus()
 
     while True:
         readQueue(oscid)
 
-
-def update_mouse(message, *args):
-    global x_min, y_min, x_max, y_max, W, H
-    y_pos, x_pos = message[2:] # value recived from the twiz
-
-    x_pos = ( (x_pos + 0.5 - x_min) / (x_max-x_min) ) * W
-    y_pos = ( (y_pos + 0.5 - y_min) / (y_max-x_min) ) * H
-    # TODO : invert y, smoothen, improve range...
-
-    M.move(x_pos, y_pos)
-    print x_pos, '\t', y_pos
-
 if __name__ == '__main__':
     main()
+
